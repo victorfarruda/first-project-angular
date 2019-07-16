7a90ced22ce05508a20d6af17abbceda0d9b30be
Victor de Freitas Arruda <victorf.arruda@outlook.com>
2019-07-16 12:24:39 -0300

    adding ingredints to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,24 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Other Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe(
+      'Bife à Milanesa',
+      'Bife a Milanesa Maravilhoso',
+      'https://img.itdg.com.br/tdg/images/recipes/000/000/055/57226/57226_original.jpg?mode=crop&width=710&height=400',
+      [
+        new Ingredient('Carne', 1.5),
+        new Ingredient('Ovos', 3),
+      ]
+    ),
+    new Recipe(
+      'Risotto',
+      'Risotto Delícioso',
+      'https://www.pingodoce.pt/wp-content/uploads/2016/02/617x370_pd_6601.jpg',
+      [
+        new Ingredient('Arroz', 0.4),
+        new Ingredient('Vinho Branco', 400),
+      ]
+    ),
   ];
 
   constructor() { }
